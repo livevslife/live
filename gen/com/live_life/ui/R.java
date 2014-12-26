1e74441dea6c7d700721797844959edc6b56e38f
qqliaoxin <qqliaoxin@qq.com>
2014-12-26 16:39:07 +0800

    更新home

--- gen/com/live_life/ui/R.java
+++ gen/com/live_life/ui/R.java
@@ -8,61 +8,68 @@
 package com.live_life.ui;
 
 public final class R {
+    public static final class anim {
+        public static final int fade_in=0x7f040000;
+        public static final int fade_out=0x7f040001;
+    }
     public static final class attr {
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
-
-         Customize dimensions originally defined in res/values/dimens.xml (such as
-         screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
-    
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int home_tab_background_selector=0x7f020000;
+        public static final int home_tab_cart_selector=0x7f020001;
+        public static final int home_tab_category_selector=0x7f020002;
+        public static final int home_tab_main_selector=0x7f020003;
+        public static final int home_tab_personal_selector=0x7f020004;
+        public static final int home_tab_search_selector=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int main_bottom_tab_cart_focus=0x7f020007;
+        public static final int main_bottom_tab_cart_normal=0x7f020008;
+        public static final int main_bottom_tab_category_focus=0x7f020009;
+        public static final int main_bottom_tab_category_normal=0x7f02000a;
+        public static final int main_bottom_tab_home_focus=0x7f02000b;
+        public static final int main_bottom_tab_home_normal=0x7f02000c;
+        public static final int main_bottom_tab_personal_focus=0x7f02000d;
+        public static final int main_bottom_tab_personal_normal=0x7f02000e;
+        public static final int main_bottom_tab_search_focus=0x7f02000f;
+        public static final int main_bottom_tab_search_normal=0x7f020010;
+        public static final int main_navigation_background=0x7f020011;
+        public static final int main_navigation_highlight_bg=0x7f020012;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090007;
+        public static final int editText1=0x7f090000;
+        public static final int home_radio_button_group=0x7f090001;
+        public static final int home_tab_cart=0x7f090005;
+        public static final int home_tab_category=0x7f090004;
+        public static final int home_tab_main=0x7f090002;
+        public static final int home_tab_personal=0x7f090006;
+        public static final int home_tab_search=0x7f090003;
     }
     public static final class layout {
-        public static final int layout_life=0x7f030000;
+        public static final int layout_index=0x7f030000;
+        public static final int layout_life=0x7f030001;
     }
     public static final class menu {
-        public static final int life=0x7f070000;
+        public static final int life=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060002;
     }
     public static final class style {
-        /** 
-        Base application theme, dependent on API level. This theme is replaced
-        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
-    
-
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
-
-        Base application theme for API 11+. This theme completely replaces
-        AppBaseTheme from res/values/styles.xml on API 11+ devices.
-    
- API 11 theme customizations can go here. 
-
-        Base application theme for API 14+. This theme completely replaces
-        AppBaseTheme from BOTH res/values/styles.xml and
-        res/values-v11/styles.xml on API 14+ devices.
-    
- API 14 theme customizations can go here. 
+        /**  去除主题的标题栏 
          */
-        public static final int AppBaseTheme=0x7f060000;
-        /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
+        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppTheme=0x7f070001;
+        /**  首页的RadioButton 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int home_tab_bottom=0x7f070002;
     }
 }
--- src/com/live_life/ui/LifeActivity.java
+++ src/com/live_life/ui/LifeActivity.java
@@ -3,12 +3,14 @@ package com.live_life.ui;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.Window;
 
 public class LifeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏
         setContentView(R.layout.layout_life);
     }
 
@@ -17,6 +19,7 @@ public class LifeActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.life, menu);
+       
         return true;
     }
     
